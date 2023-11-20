b7c869679119e775ae856c5501e0679521661f18
rebeca <beca.cout@gmail.com>
2023-11-20 10:24:42 -0300

    load 3d model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,45 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+
 function Home() {
-  return <section className=" w-full h-full relative"></section>;
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+  return (
+    <section className="w-full h-full relative">
+      <Canvas
+        className="w-full h-full bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight intensity={0} />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Island
+            scale={islandScale}
+            rotation={islandRotation}
+            position={islandPosition}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
 }
 
 export default Home;
